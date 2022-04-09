f0eeac5c5f22a3efd536b19cb073d6e7ab782e6d
Christ Kamanda <kamandachristo@gmail.com>
2022-04-09 18:14:52 -0400

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+
 // Code your solution here
+const drivers = [`Bobby`, `Sammy`, `Sally`, `Annette`, `Sarah`, `Bobby`]
+function findMatching(drivers, string){
+    return drivers.filter(elem => elem.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string){
+    const findMatch = drivers.filter(function(drivers){
+        if(string === drivers.slice(0,2)){
+            return(drivers);
+        }
+        else{
+            return drivers === string
+        }
+    })
+    return findMatch
+}
+
+function matchName(drivers, string){
+
+    const objetName = drivers.filter(function(string){
+        for(let object in drivers){
+            if(string === drivers[0]){
+                return drivers
+            }
+            else{
+                return string === drivers[4]
+            }
+        }
+    });
+    return objetName
+}
+matchName(drivers, `Bobby`);
+
+
